6a7182af3c70c9a24591da9a831939ff13f83a43
Dr. Samuel Israel <samuelisraelpm@gmail.com>
2026-05-12 15:07:47 -0700

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import plotly.express as px
-
+import plotly.graph_objects as go
 # Page configuration
 st.set_page_config(
     page_title="🦟 Climate-Aware Malaria Prediction & Explainable Healthcare AI System",
